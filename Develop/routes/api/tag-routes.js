2997bafc73a96cb22d3d6465001b4aceb37bfde9
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-11 22:02:57 -0600

    Added put code for tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -44,8 +44,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  if (!req.body.tag_name) {
+    res.status(304).json();
+    return;
+  }
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: 'No tag found with that ID' });
+      return;
+    }
+    res.status(200).json('Tag name has been updated');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
